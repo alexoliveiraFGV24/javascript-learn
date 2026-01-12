7925282403d2fe920d80438188690fa57f4de861
Alex Maia <iamsralex@gmail.com>
2026-01-12 15:31:03 -0300

    Simplificando código

--- class4/ex1/exerc-fix-node-02.js
+++ class4/ex1/exerc-fix-node-02.js
@@ -17,28 +17,23 @@ while (true) {
     const habitantes = Number(await rl.question("Digite o número de habitantes da sua cidade: "));
 
     // Validações
-    if (!Number.isNaN(habitantes) && Number.isInteger(habitantes) ) {
-        if (habitantes < 0) {
-            console.log("\nErro: Digite um número inteiro não negativo");
+    if (!Number.isNaN(habitantes) && Number.isInteger(habitantes) && habitantes > 0) {
+
+        // Operações com if. else if e else
+        if (habitantes <= 100000) {
+            console.log(mensagem + "cidade de pequeno porte.");
+        } else if (habitantes <= 500000) {
+            console.log(mensagem + "cidade de médio porte.");
+        } else if (habitantes <= 1000000) {
+            console.log(mensagem + "cidade de grande porte.");
         } else {
-
-            // Operações com if. else if e else
-            if (habitantes <= 100000) {
-                console.log(mensagem + "cidade de pequeno porte.");
-                process.exit(0);
-            } else if (habitantes <= 500000) {
-                console.log(mensagem + "cidade de médio porte.");
-                process.exit(0);
-            } else if (habitantes <= 1000000) {
-                console.log(mensagem + "cidade de grande porte.");
-                process.exit(0);
-            } else {
-                console.log(mensagem + "megacidade.");
-                process.exit(0);
-            }
-
+            console.log(mensagem + "megacidade.");
         };
+
+        // Encerrando o processo
+        process.exit(0);  // Nesse caso, equivale a rl.close();
+
     } else {
-        console.log("\nErro: Digite um número inteiro não negativo");
+        console.log("\nErro: Digite um número inteiro positivo");
     };
 };
