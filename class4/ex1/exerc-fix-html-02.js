8d1d40e3658f85de8bb8fd91420f6462e8d3f5a1
Alex Maia <iamsralex@gmail.com>
2026-01-12 14:49:56 -0300

    Adicionando comentários e o fluxograma

--- class4/ex1/exerc-fix-html-02.js
+++ class4/ex1/exerc-fix-html-02.js
@@ -1,13 +1,16 @@
-const display = document.getElementById("resultado");
-let mensagem = "A cidade é classificada como";
+const display = document.getElementById("resultado");  // Pegando a div para colocar os resultados
+let mensagem = "A cidade é classificada como";  // Mensagem padrão
 
-
+// Pegando o input do usuário
 const habitantes = Number(prompt("Digite o número de habitantes da sua cidade: "));
+
+// Validações
 if (!Number.isNaN(habitantes) && Number.isInteger(habitantes) ) {
     if (habitantes < 0) {
         display.innerHTML = `<p style="color: red;">Erro: Digite um número inteiro não negativo</p>`;
     } else {
-
+        
+        // Operações com if. else if e else
         if (habitantes <= 100000) {
             display.innerHTML = `<p>${mensagem} cidade de pequeno porte.</p>`;
         } else if (habitantes <= 500000) {
--- class4/ex1/exerc-fix-node-02.js
+++ class4/ex1/exerc-fix-node-02.js
@@ -1,22 +1,28 @@
 import readline from "node:readline/promises";
 
-
+// Fazendo a estrutura do readline
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+// Criando a base da mensagem
 let mensagem = "A cidade é classificada como ";
 
 
+// Fazendo um loop para classificar os habitantes
 while (true) {
+
+    // Input do usuário
     const habitantes = Number(await rl.question("Digite o número de habitantes da sua cidade: "));
 
+    // Validações
     if (!Number.isNaN(habitantes) && Number.isInteger(habitantes) ) {
         if (habitantes < 0) {
             console.log("\nErro: Digite um número inteiro não negativo");
         } else {
 
+            // Operações com if. else if e else
             if (habitantes <= 100000) {
                 console.log(mensagem + "cidade de pequeno porte.");
                 process.exit(0);
