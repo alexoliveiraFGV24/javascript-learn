c283c0a4cb9f6461aad1f22991015b5b3d3f7de8
Alex Maia <iamsralex@gmail.com>
2026-01-13 14:37:52 -0300

    Adicionando mais um exercício

---
+++ class4/ex3/teste-html.js
@@ -0,0 +1,85 @@
+// Definindo o display para imprimir os resultados
+const display = document.getElementById("resultado");
+
+// Função para pedir um número inteiro válido
+function numeroInteiroValido(mensagem) {
+    while (true) {
+
+        // Input do usuário
+        const input = prompt(mensagem);
+        const num = Number(input);
+
+        // Validação
+        if (input !== null && input !== "" && !Number.isNaN(num) && Number.isInteger(num) && num > 0) {
+            return num;
+        }
+        
+        // Exibe erro temporário se quiser, mas o prompt abrirá novamente
+        alert("Erro: Você precisa digitar um número inteiro positivo válido!");
+        };
+};
+
+// Função para retornar um valor formatado
+function formatarNumero (valor) {
+    if (valor === Number.MAX_SAFE_INTEGER || valor === Number.MIN_SAFE_INTEGER) {
+        return null;
+    } else {
+        return valor;
+    };
+};
+
+// Definindo as variáveis (você pode mudar Number.MIN_SAFE_INTEGER e Number.MAX_SAFE_INTEGER para -Infinity e Infinity)
+const n = numeroInteiroValido("Digite a quantidade de números: ");
+let nums = [];
+let pares = [];
+let impares = [];
+let maior_par = Number.MIN_SAFE_INTEGER;
+let maior_impar = Number.MIN_SAFE_INTEGER;
+let menor_par = Number.MAX_SAFE_INTEGER;
+let menor_impar = Number.MAX_SAFE_INTEGER;
+
+// Pegando os números e separando em pares e ímpares, além de mudar os maiores e menores
+for (let i = 0; i < n; i++) {
+    
+   const num = numeroInteiroValido(`Digite o número ${i+1}: `);
+
+   nums.push(num);
+
+   if (num % 2 === 0) {
+      pares.push(num);
+      if (num < menor_par) {
+         menor_par = num;
+      };
+      if (num > maior_par) {
+         maior_par = num;
+      };
+
+   } else {
+      impares.push(num);
+      if (num < menor_impar) {
+         menor_impar = num;
+      };
+      if (num > maior_impar) {
+         maior_impar = num;
+      };
+   };
+
+};
+
+// Imprimindo os resultados
+const conteudo = `
+    <h3>Resultados</h3>
+    <p><strong>Números digitados:</strong> ${nums}</p>
+    <hr>
+    <p><strong>Pares:</strong> [${pares}]</p>
+    <ul>
+        <li>Menor par: ${formatarNumero(menor_par)}</li>
+        <li>Maior par: ${formatarNumero(maior_par)}</li>
+    </ul>
+    <p><strong>Ímpares:</strong> [${impares}]</p>
+    <ul>
+        <li>Menor ímpar: ${formatarNumero(menor_impar)}</li>
+        <li>Maior ímpar: ${formatarNumero(maior_impar)}</li>
+    </ul>
+`;
+display.innerHTML = conteudo;
---
+++ class4/ex3/teste.js
@@ -0,0 +1,76 @@
+import readline from "node:readline/promises";
+
+// Função para pedir um número inteiro válido
+async function numeroInteiroValido(mensagem) {
+    while (true) {
+        const num = Number(await rl.question(mensagem));
+        if (!Number.isNaN(num) && Number.isInteger(num) && num > 0) {
+            return num;
+        };
+        console.log(`\nErro: Você precisa digitar um número inteiro positivo válido!`);
+    };
+};
+
+// Função para retornar um valor formatado
+function formatarNumero (valor) {
+    if (valor === Number.MAX_SAFE_INTEGER || valor === Number.MIN_SAFE_INTEGER) {
+        return null;
+    } else {
+        return valor;
+    };
+};
+
+// Criando a interface para input de teclado
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout 
+});
+
+// Definindo as variáveis
+const n = await numeroInteiroValido("Digite a quantidade de números: ");
+let nums = [];
+let pares = [];
+let impares = [];
+let maior_par = Number.MIN_SAFE_INTEGER;
+let maior_impar = Number.MIN_SAFE_INTEGER;
+let menor_par = Number.MAX_SAFE_INTEGER;
+let menor_impar = Number.MAX_SAFE_INTEGER;
+
+// Pegando os números e separando em pares e ímpares, além de mudar os maiores e menores
+for (let i = 0; i < n; i++) {
+   const num = await numeroInteiroValido(`Digite o número ${i+1}: `);
+
+   nums.push(num);
+
+   if (num % 2 === 0) {
+      pares.push(num);
+      if (num < menor_par) {
+         menor_par = num;
+      };
+      if (num > maior_par) {
+         maior_par = num;
+      };
+
+   } else {
+      impares.push(num);
+      if (num < menor_impar) {
+         menor_impar = num;
+      };
+      if (num > maior_impar) {
+         maior_impar = num;
+      };
+   };
+
+};
+
+// Fechando a interface de input
+rl.close();
+
+// Imprimindo os resultados
+console.log("\nNúmeros digitados: " + nums);
+console.log("Números pares: " + pares);
+console.log("Números ímpares: " + impares);
+console.log("Menor par: " + formatarNumero(menor_par));
+console.log("Menor ímpar: " + formatarNumero(menor_impar));
+console.log("Maior par: " + formatarNumero(maior_par));
+console.log("Maior ímpar: " + formatarNumero(maior_impar));
