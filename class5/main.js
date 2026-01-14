fbae483494fe884dc077b4ba127b03ae6872f461
Alex Maia <iamsralex@gmail.com>
2026-01-14 17:00:12 -0300

    Iniciando o projeto de vendas

---
+++ class5/main.js
@@ -0,0 +1,12 @@
+// Initialize
+// Mostra o menu
+// Rodar o processamento do CRUD com input ou com aleatório
+// Sair 
+
+// Buscar cliente estará em listar clientes
+// Buscar produto estará em listar produtos
+
+const clientes = new Map();
+const produtos = new Map();
+const vendas = new Map();
+const itensVenda = new Map();
